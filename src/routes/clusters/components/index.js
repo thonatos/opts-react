5864341527e3c384e7546b9c48a6130c6da12a0b
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-17 16:12:35 +0800

    feat: move app/service comp to clustersDetail

--- src/routes/clusters/components/index.js
+++ src/routes/clusters/components/index.js
@@ -1,5 +1,3 @@
 import ClusterTable from './clusterTable'
-import AppTable from './appTable'
-import ServiceTalbe from './serviceTalbe'
 
-export { AppTable, ClusterTable, ServiceTalbe }
+export { ClusterTable }
