1c4e4f3bdfa6dfe738c14764194d15201870e8b3
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-17 18:05:34 +0800

    feat: add UserInfo comp

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import CodeEditor from './codeEditor'
 import LoginForm from './loginForm'
 import SideMenu from './sideMenu'
 import Protected from './protected'
+import UserInfo from './userInfo'
 
-export { Protected, SideMenu, CodeEditor, LoginForm }
+export { Protected, SideMenu, CodeEditor, LoginForm, UserInfo }
