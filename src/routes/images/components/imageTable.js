e219c1a93a3545b4945daad758a720e2013dc101
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-29 15:58:55 +0800

    refactor: clean code

--- src/routes/images/components/imageTable.js
+++ src/routes/images/components/imageTable.js
@@ -1,30 +1,6 @@
 import React, { Component } from 'react'
 import { Table } from 'antd'
-// import { Table, Tag } from 'antd'
 import moment from 'moment'
-
-// const TAG_COLORS = ['#87d068', '#2db7f5', '#108ee9']
-// const expandedRowRender = ({ tags }) => {
-//   const imageTags = tags.length > 0 ? tags.peek() : []
-//   return (
-//     <div>
-//       <ul>
-//         {imageTags.map(t => {
-//           const { id, tag, digest, pushed_at } = t
-//           const ramdomColor = TAG_COLORS[id % 3]
-//           const pushed = moment(pushed_at).format('MM/DD/YYYY HH:mm:ss')
-//           return (
-//             <li key={id} style={{ marginTop: '0.4em' }}>
-//               <div>
-//                 <Tag color={ramdomColor}>{tag}</Tag> {digest} - {pushed}
-//               </div>
-//             </li>
-//           )
-//         })}
-//       </ul>
-//     </div>
-//   )
-// }
 
 class ImageTable extends Component {
   onChange = (pagination, filters, sorter) => {
