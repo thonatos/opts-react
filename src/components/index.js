8dd06bcd4a0b40f608442a72cce664a2a9a1fe6b
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-03-24 04:05:21 +0800

    refactor: rename files

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import CodeEditor from './codeEditor'
-import LoginForm from './loginForm'
-import SideMenu from './sideMenu'
+import CodeEditor from './code_editor'
+import LoginForm from './login_form'
+import SideMenu from './side_menu'
 import Protected from './protected'
-import UserInfo from './userInfo'
+import UserInfo from './user_info'
 
 export { Protected, SideMenu, CodeEditor, LoginForm, UserInfo }
